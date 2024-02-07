7ecdc47e402042c4fdfcde9678ffe8d3dc15d9c1
15thatsawan <152370453+15thatsawan@users.noreply.github.com>
2024-02-07 16:50:40 +0700

    update Webboard index.php

--- index.php
+++ index.php
@@ -6,28 +6,49 @@ session_start();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="bootstrap-5.3.2-dist/css/bootstrap.min.css">
+    <script src="bootstrap-5.3.2-dist/js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <title>Webboard</title>
 </head>
 <body>
-    <h1 style="text-align: center;">Webboard KakKak</h1>
-    <hr>
+    <div class="container-lg">
+    <h1 style="text-align: center;" class="mt-3">Webboard KakKak</h1>
+    
+
+    <nav class="navbar navbar-expand-lg " style="background-color: #d3d3d3;">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="index.php"><i class="bi bi-house-door-fill"></i> Home</a>
+            <ul class="navbar-nav">
+            <?php if(!isset($_SESSION['id'])){ ?>
+                <li class="nav-item">
+                    <a class="nav-link " aria-current="page" href="login.php"><i class="bi bi-pencil-square"></i> เข้าสู่ระบบ</a>
+                </li>
+            <?php }else{ ?>
+                <li class="nav-item dropdown">
+                    <a class="btn btn-outline-secondary btn-sm dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        <i class="bi bi-person-lines-fill"></i> <?php echo $_SESSION['username'];?>
+                    </a>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="logout.php"><i class="bi bi-power"></i> ออกจากระบบ</a></li>
+                        
+                    </ul>
+                </li>
+            <?php  } ?>
+            
+                
+                
+            </ul>
+        </div>
+    </nav>
+
     <form action="#">
         หมวดหมู่ : <select >
                 <option value="all">--ทั้งหมด--</option>
                 <option value="general">เรื่องทั่วไป</option>
                 <option value="study">เรื่องเรียน</option>
         </select> 
-        <?php
-        if(isset($_SESSION['id'])){
-            echo "<div style='float: right';>
-            ผู้ใช้งานระบบ : $_SESSION[username]&nbsp;&nbsp;
-            <a href=logout.php >ออกจากระบบ</a> 
-            </div><br><a href=newpost.php>สร้างกระทู้ใหม่</a>";// ใน php style ต้องใช้ '' คุม
-       
-        }else{
-            echo "<a href=login.php style='float: right';>เข้าสู่ระบบ</a>";
-        }
-        ?>
+        
     </form>
     <ul > 
         <?php
@@ -45,12 +66,7 @@ session_start();
    
 
     ?>
-        <!-- <li><a href="post.php?id=1" >กระทู้ที่ 1</a></li>
-        <li><a href="post.php?id=2" >กระทู้ที่ 2</a></li>
-        <li><a href="post.php?id=3" >กระทู้ที่ 3</a></li>
-        <li><a href="post.php?id=4" >กระทู้ที่ 4</a></li>
-        <li><a href="post.php?id=5" >กระทู้ที่ 5</a></li> -->
     </ul>
-    
+    </div>
 </body>
 </html>
